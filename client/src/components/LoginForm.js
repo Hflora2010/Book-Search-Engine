55a61a975d7f6f3db32461f023f3f6c628403224
Hailey Flora <Haileycflora@gmail.com>
2023-06-24 19:33:13 -0400

    updates to LoginForm.js

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -35,7 +35,7 @@ const LoginForm = () => {
         variables: { userFormData },
       });
 
-      Auth.login(data.loginUser.token);
+      Auth.login(data.login.token);
     } catch (err) {
       console.error(error);
       setShowAlert(true);
