2642e0a6d30a63721c54dc5213b2e971b918ff3b
Hailey Flora <Haileycflora@gmail.com>
2023-06-23 23:03:36 -0400

    add @apollo/client to client

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useParams } from "react-router-dom";
 import { useQuery } from "@apollo/client";
 import { GET_ME } from "../utils/queries"; 
-import { REMOVE_BOOK} from "../utils/queries";
+import { REMOVE_BOOK} from "../utils/mutations";
 import {
   Container,
   Card,
